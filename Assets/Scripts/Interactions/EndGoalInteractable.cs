d6706a33b0cc7d97aa9c0447712e7eceb7ec7d92
Iain Skaggs <iain.skaggs@gmail.com>
2025-08-03 14:57:39 +0100

    Set up End Goal interactable object and animation

---
+++ Assets/Scripts/Interactions/EndGoalInteractable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Interactions
+{
+    public class EndGoalInteractable : Interactable
+    {
+        [SerializeField] private SkinnedMeshRenderer _renderer;
+        [SerializeField] private Material _activeMat;
+        [SerializeField] private AnimationClip _activateAnim;
+        
+        private Animator _anim;
+        private bool _isGameComplete = false;
+
+        private void Start()
+        {
+            _anim = GetComponent<Animator>();
+        }
+
+        public void SetReadyForCompletion()
+        {
+            triggered = false;
+            _isGameComplete = true;
+        }
+
+        public override bool Interact(bool status)
+        {
+            if (_isGameComplete)
+            {
+                isEnabled = false;
+                _renderer.material = _activeMat;
+                _anim.Play(_activateAnim.name);
+                StartCoroutine(_activateOnDelay());
+                return true;
+            }
+            else
+            {
+                return base.Interact(status);
+            }
+        }
+
+        private IEnumerator _activateOnDelay()
+        {
+            yield return new WaitForSeconds(_activateAnim.length + 1f);
+            base.Interact(true);
+        }
+    }
+}
