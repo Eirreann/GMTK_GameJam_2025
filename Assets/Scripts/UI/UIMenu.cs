cc99098d18b44ac93a0cdd7d467e4303c3b502d5
Iain Skaggs <iain.skaggs@gmail.com>
2025-08-03 16:29:18 +0100

    Minor update to player HUD and landing UI, do test build and generate associated assets

--- Assets/Scripts/UI/UIMenu.cs
+++ Assets/Scripts/UI/UIMenu.cs
@@ -8,22 +8,25 @@ public class UIMenu : MonoBehaviour
     [Header("Panels")]
     public GameObject SettingsPanel;
     public GameObject CreditsPanel;
+    public GameObject IntroPanel;
     
     [Header("Buttons")]
     public Button StartBtn;
     public Button SettingsBtn;
     public Button CreditsBtn;
+    public Button ContinueBtn;
 
     private void Start()
     {
         StartBtn.onClick.AddListener(_start);
         SettingsBtn.onClick.AddListener(_settings);
         CreditsBtn.onClick.AddListener(_credits);
+        ContinueBtn.onClick.AddListener(_continue);
     }
 
     private void _start()
     {
-        SceneManager.LoadScene(1);
+        IntroPanel.SetActive(true);
     }
 
     private void _settings()
@@ -36,4 +39,8 @@ public class UIMenu : MonoBehaviour
         CreditsPanel.SetActive(true);
     }
 
+    private void _continue()
+    {
+        SceneManager.LoadScene(1);
+    }
 }
